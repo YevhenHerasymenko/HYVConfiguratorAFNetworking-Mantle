3393649b4f8ddd6ae3a38a71e45d3fab008faccc
Gerasymenko Yevgen <fess2036@ya.ru>
2015-01-19 10:08:07 +0200

    Release 0.0.1.

---
+++ Pod/Classes/HYVConfiguratorAFNetworking-Mantle.m
@@ -0,0 +1,13 @@
+//
+//  HYVConfiguratorAFNetworking-Mantle.m
+//  Pods
+//
+//  Created by Gerasymenko Yevgen on 19.01.15.
+//
+//
+
+#import "HYVConfiguratorAFNetworking-Mantle.h"
+
+@implementation HYVConfiguratorAFNetworking_Mantle
+
+@end
