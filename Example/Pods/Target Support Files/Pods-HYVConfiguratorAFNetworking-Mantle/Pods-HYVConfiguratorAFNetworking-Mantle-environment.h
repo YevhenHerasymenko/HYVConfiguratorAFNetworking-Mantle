d4bd2750ed52a6db9c0d9df75b679ef384c72f12
Gerasymenko Yevgen <fess2036@ya.ru>
2015-01-19 09:56:40 +0200

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-HYVConfiguratorAFNetworking-Mantle/Pods-HYVConfiguratorAFNetworking-Mantle-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// HYVConfiguratorAFNetworking-Mantle
+#define COCOAPODS_POD_AVAILABLE_HYVConfiguratorAFNetworking_Mantle
+#define COCOAPODS_VERSION_MAJOR_HYVConfiguratorAFNetworking_Mantle 0
+#define COCOAPODS_VERSION_MINOR_HYVConfiguratorAFNetworking_Mantle 1
+#define COCOAPODS_VERSION_PATCH_HYVConfiguratorAFNetworking_Mantle 0
+
